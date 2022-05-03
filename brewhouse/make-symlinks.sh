72a269468dd51bb5ff4f7f759516934ad622c8af
Neil Sweeney <neil@wolfiezero.com>
2022-05-03 12:16:16 +0100

    Update symlink locations

--- brewhouse/make-symlinks.sh
+++ brewhouse/make-symlinks.sh
@@ -9,7 +9,7 @@ source ~/.dotfiles/brewhouse/_config.sh
 ln -s ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/System/ssh ~/.ssh
 
 # rm -rf ~/.gitconfig
-ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
 
 # rm -rf ~/.gitignore
-ln -s ~/dotfiles/git/gitignore ~/.gitignore
+ln -s ~/.dotfiles/git/.gitignore ~/.gitignore
