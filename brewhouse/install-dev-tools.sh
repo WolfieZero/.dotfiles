c708b7d46a77aac881d64754865b6861415cb941
Neil Sweeney <neil@wolfiezero.com>
2022-05-17 23:26:15 +0100

    Update dev apps

--- brewhouse/install-dev-tools.sh
+++ brewhouse/install-dev-tools.sh
@@ -6,7 +6,7 @@ source ~/.dotfiles/brewhouse/_config.sh
 # ------------------------------------------------------------------------------
 
 apps=(
-  git                   # override mac's
+  # git                   # override mac's
   iterm2                # terminal
   imagealpha               # optimise transparent pngs
   imageoptim               # images
@@ -21,11 +21,10 @@ apps=(
   # sequel-ace            # mysql/mariadb tool
   # sqlectron             # generic db tool
 
-  #Browser
-  browserosaurus           # browser picker
-  homebrew/cask-versions/firefox-developer-edition
+  # Browser
+  # homebrew/cask-versions/firefox-developer-edition
   # homebrew/cask-versions/google-chrome-dev
-  google-chrome
+  # google-chrome
 
   # Design
   figma
@@ -40,3 +39,4 @@ brew_install_multiple "${apps[@]}"
 echo 'App Store Installables'
 echo '----------------------'
 echo ' - Sysytem Color Picker  https://apps.apple.com/gb/app/system-color-picker/id1545870783?mt=12'
+echo ' - Timemator Basic       https://apps.apple.com/gb/app/timemator-basic/id1458451198?mt=12
